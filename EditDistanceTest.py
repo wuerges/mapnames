d596c3a03395973fd6a00e1aa37b1d4c63dcee0a
Gabriel Galli <g7.galli96@gmail.com>
2018-04-08 23:37:54 -0300

    Initial README and Wagner-Fischer code

---
+++ EditDistanceTest.py
@@ -0,0 +1,40 @@
+import tcc
+import string
+import unittest as ut
+
+
+class EditDistanceTest(ut.TestCase):
+
+    def test_identity(self):
+        for i in range(len(string.digits)):
+            id = string.digits[:i]
+            self.assertEqual(tcc.wagner_fischer(id, id), 0)
+
+    def test_one_empty(self):
+        for i in range(len(string.digits)):
+            s = string.digits[:i]
+            self.assertEqual(tcc.wagner_fischer('', s), i)
+            self.assertEqual(tcc.wagner_fischer(s, ''), i)
+
+    def test_ascending(self):
+        for i in range(len(string.digits)):
+            s1 = string.digits[:i]
+            for j in range(len(string.digits)):
+                s2 = string.digits[:j]
+                self.assertEqual(tcc.wagner_fischer(s1, s2), abs(i - j))
+
+    def test_misc(self):
+        self.assertEqual(tcc.wagner_fischer('abc', 'b'), 2)
+        self.assertEqual(tcc.wagner_fischer('abc', 'b'), 2)
+        self.assertEqual(tcc.wagner_fischer('abc', 'ac'), 1)
+        self.assertEqual(tcc.wagner_fischer('abc', 'bc'), 1)
+        self.assertEqual(tcc.wagner_fischer('abcdefg', 'abce'), 3)
+        # Different from last case because of symmetric 'd'
+        self.assertEqual(tcc.wagner_fischer('abcdefg', 'gfedcba'), 6)
+        # '0123456789' and '9876543210'
+        self.assertEqual(tcc.wagner_fischer(string.digits, string.digits[::-1]),
+                         len(string.digits))
+
+
+if __name__ == '__main__':
+    ut.main()
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # mapnames
+
+This is the repository for the code of my Computer Science bachelor's degree final paper. Pretty much undocumented for now.
