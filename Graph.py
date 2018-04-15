68bf1db3e0066b4bca2a0058e4df0fdb55890f2d
Gabriel Galli <g7.galli96@gmail.com>
2018-04-15 02:28:44 -0300

    Implemented Irving's weakly stable marriage algorithm

---
+++ Graph.py
@@ -0,0 +1,68 @@
+import copy as cp
+import numpy as np
+
+
+class Vertex():
+    def __init__(self, label):
+        # edges list not necessary
+        # because of preferences list
+        self.label = label
+        self.prefs = None
+
+
+class BipartiteGraph():
+    def __init__(self, U, V):
+        self.U = U
+        self.V = V
+        self.matching = None
+
+    # Irving weakly stable marriage algorithm
+    # which is an extension to Gale-Shapley's
+    def stable_match(self):
+        self.matching = {}
+        # deep copy so we don't change
+        # original vertices data
+        free_men = set(cp.deepcopy(self.U))
+        while len(free_men) > 0:
+            m = free_men.pop()
+            w = m.prefs[0]
+
+            # if some man p is engaged to w,
+            # set him free
+            p = self.matching.get(w)
+            if p is not None:
+                del self.matching[p]
+                free_men.add(p)
+
+            # m engages w
+            self.matching[m] = w
+            self.matching[w] = m
+
+            # for each successor m' of m on w's preferences,
+            # remove w from m's preferences so that no man
+            # less desirable than m will propose w
+            for i in range(w.prefs.index(m) + 1, len(w.prefs)):
+                w.prefs[i].prefs.remove(w)
+
+
+if __name__ == '__main__':
+    u1 = Vertex('u1')
+    u2 = Vertex('u2')
+
+    v1 = Vertex('v1')
+    v2 = Vertex('v2')
+
+    u1.prefs = [v1, v2]
+    u2.prefs = [v1, v2]
+
+    v1.prefs = [u2, u1]
+    v2.prefs = [u2, u1]
+
+    U = set([u1, u2])
+    V = set([v1, v2])
+    G = BipartiteGraph(U, V)
+    G.stable_match()
+
+    print('Resulting stable matching:')
+    for (m, w) in G.matching.items():
+        print(f'({m.label}, {w.label})')
