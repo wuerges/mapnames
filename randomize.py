7a8e5f8223a590f483fcf74d4af1425d865715a1
Gabriel Galli <g7.galli96@gmail.com>
2018-05-10 01:51:27 -0300

    Added tool to randomly extract samples from test cases

---
+++ randomize.py
@@ -0,0 +1,20 @@
+import sys
+import json
+import random
+
+
+def main():
+    if len(sys.argv) < 3:
+        print(
+            f'Missing arguments. Usage:\npython {sys.argv[0]} <json file> <output size>')
+        exit(0)
+    with open(sys.argv[1], 'r') as f:
+        length = int(sys.argv[2])
+        map = json.load(f)
+        samples = random.sample(map.items(), length)
+        output = {k: v for (k, v) in samples}
+        print(json.dumps(output, indent=4))
+
+
+if __name__ == '__main__':
+    main()
